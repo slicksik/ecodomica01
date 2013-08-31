8e38f219cc1647b68cefc7185b7aab5109ab0103
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2013-08-31 20:04:47 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecodomica01::Application.config.secret_token = '9fe14779cf719602a2c000f57ab8922d568fe59fd4f8919298af9f06d5027860e19ed6ba11c03d04e2137e9b712dfadc579a68b1a3fe0676cd035f08606006d4'
